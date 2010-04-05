d519e9aba48c3b547588d6f06fb662d44875bfac
Kenneth Reitz <me@kennethreitz.com>
2010-04-04 23:23:21 -0400

    fixed auth keys location (thanks, sivel)

--- blog/wp-config.php
+++ blog/wp-config.php
@@ -39,12 +39,6 @@ define('DB_COLLATE', '');
 $table_prefix  = 'wp_';
 define ('WPLANG', 'English');
 
-if ( !defined('ABSPATH') )
-	define('ABSPATH', dirname(__FILE__) . '/');
-
-require_once(ABSPATH . 'wp-settings.php');
-
-
 
 /**#@+
  * Authentication Unique Keys.
@@ -60,3 +54,8 @@ define('SECURE_AUTH_KEY', 'put your unique phrase here');
 define('LOGGED_IN_KEY', 'put your unique phrase here');
 define('NONCE_KEY', 'put your unique phrase here');
 /**#@-*/
+
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(__FILE__) . '/');
+
+require_once(ABSPATH . 'wp-settings.php');
